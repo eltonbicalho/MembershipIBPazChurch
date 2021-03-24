7df3a882ce4e4ba108500dc64819d6fe3d48daff
Elton Bicalho <eltonbicalho@users.noreply.github.com>
2021-03-24 19:42:08 -0400

    EMAIL SERVICE PROJECT

---
+++ Application/Membership.EmailService/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Membership.EmailService
+{
+    public interface IEmailSender
+    {
+        void SendEmail(Message message);
+        Task SendEmailAsync(Message message);
+    }
+}
